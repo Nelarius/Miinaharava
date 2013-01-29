a7c1fe28e6961ff2a6dda3180540f26aaf12eff7
Johann Muszynski <johann.muszynski@gmail.com>
2013-01-29 11:03:50 +0200

    added files and folders for cmake functionality

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "engine/App.h"
 #include "UnitTest++.h"
+#include <config.h>
 
 #include <iostream>
 
@@ -8,8 +9,10 @@ int main(int argc, char** argv)
     #ifdef DEBUG
     //before running the app, run the tests:
     UnitTest::RunAllTests();
-    std::cout << std::endl;
+
+    std::cout << std::endl << "Version " << myproject_VERSION_MAJOR << "." << myproject_VERSION_MINOR << std::endl << std::endl;
     #endif // DEBUG
+
 
     App* app = App::getInstance();
     app->execute();
